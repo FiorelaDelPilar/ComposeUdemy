51022bdd2ccb0b9fb19ab70f72d7e0c2fdcb68b6
fvargasi <fvargasi@emeal.nttdata.com>
2023-11-02 15:41:40 -0500

    137 Primera función composable

--- app/src/main/java/com/example/composeudemy/MainActivity.kt
+++ app/src/main/java/com/example/composeudemy/MainActivity.kt
@@ -5,12 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hola Mundo")
+            Saludar("Tara")
         }
     }
+
+}
+
+@Composable
+fun Saludar(nombre: String) {
+    Text(text = "Hola $nombre")
+}
+
+@Preview
+@Composable
+fun PreviewText() {
+    Saludar("Tara")
 }
