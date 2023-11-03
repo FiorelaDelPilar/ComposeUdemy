059653256009a7af0ebfce2342b28938f8295219
fvargasi <fvargasi@emeal.nttdata.com>
2023-11-02 19:35:18 -0500

    145 Navegando entre composables 2/3

--- app/src/main/java/com/example/composeudemy/Navigation.kt
+++ app/src/main/java/com/example/composeudemy/Navigation.kt
@@ -4,17 +4,24 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.composeudemy.data.fruitList
+import com.example.composeudemy.ui.screen.FruitScreen
 import com.example.composeudemy.ui.screen.MasterScreen
 
-sealed class  Screen(val route:String){
-    object MasterScreen: Screen(route = "master")
+sealed class Screen(val route: String) {
+    object MasterScreen : Screen(route = "master")
+    object FruitScreen : Screen(route = "fruit_screen")
 }
+
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination =  Screen.MasterScreen.route){
-        composable(Screen.MasterScreen.route){
+    NavHost(navController = navController, startDestination = Screen.MasterScreen.route) {
+        composable(Screen.MasterScreen.route) {
             MasterScreen(navController)
+        }
+        composable(Screen.FruitScreen.route) {
+            FruitScreen(navController, fruitList)
         }
     }
 }
--- app/src/main/java/com/example/composeudemy/ui/screen/MasterScreen.kt
+++ app/src/main/java/com/example/composeudemy/ui/screen/MasterScreen.kt
@@ -1,9 +1,37 @@
 package com.example.composeudemy.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.composeudemy.Screen
 
 @Composable
 fun MasterScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navController.navigate(Screen.FruitScreen.route) }) {
+            Text(text = "Go to fruits page")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MasterPreview() {
+    MasterScreen(navController = rememberNavController())
 
 }
